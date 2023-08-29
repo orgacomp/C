159ce8715ac3ada07ef1f4d2d50d90774100f002
melijauregui <mjauregui@fi.uba.ar>
2023-08-29 17:19:51 -0300

    if_else

--- ejemplos/if_else.c
+++ ejemplos/if_else.c
@@ -6,21 +6,16 @@ int main() {
     printf("Ingrese un número: ");
     scanf("%d", &numero); //lee los datos ingresados desde la entrada estándar 
 
-    if (numero % 2 == 0) {
-        printf("El número %d es par.\n", numero);
-    } else {
-        printf("El número %d es impar.\n", numero);
-    }
-
-    printf("Además el número es ");
+    printf("El número %d es ", numero);
 
     if (numero > 0) {
         printf("positivo.\n");
     } else if (numero < 0) {
         printf("es negativo.\n");
     } else {
-        printf("El número es cero.\n");
+        printf("cero.\n");
     }
+     printf("Además es %s. \n", numero % 2 == 0? "par" : "impar");
 
     return 0;
 }
